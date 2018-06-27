9e95f195e0fd1394b8e18c602291ca142607bab9
caffedrine <bacchusstyle@yahoo.com>
2018-06-27 21:11:38 +0300

    works now on multithreading

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from util import *
 # GLOBALS
 contest_id = "22"
 vote_id = "207"
-requestUrl = "https://www.freerider.ro/wp-admin/admin-ajax.php?vote_id={id}".format(id=vote_id)
+request_url = "https://www.freerider.ro/wp-admin/admin-ajax.php?vote_id={id}".format(id=vote_id)
 
 # Proxy list file and array()
 proxy_list = "proxy.txt"
@@ -21,7 +21,7 @@ proxy_array = []
 
 
 # Function used to send vote
-def try_vote(contest_id, vote_id, req_url, attempts=1, proxy=None, proxy_type=None):
+def try_vote(contest_id, vote_id, req_url, attempts=2, proxy=None):
     # Web requests session
     web_session = requests.Session()  # type: Session
 
@@ -58,38 +58,40 @@ def try_vote(contest_id, vote_id, req_url, attempts=1, proxy=None, proxy_type=No
                     "some_str": "099109b4a7",
                     "ds": "MTg3OXgxMDU2"}
 
-    # Proxy tuple
-    curr_proxy_tuple = None
-    if proxy_type is not 'None':
-        curr_proxy_tuple = {"https": "{proxy_type}://{ip_port}".format(proxy_type=proxy_type.lower(), ip_port=proxy),
-                            "http": "{proxy_type}://{ip_port}".format(proxy_type=proxy_type.lower(), ip_port=proxy)}
-
     # Store response
     function_resp = {"status": False, "error_desc": ""}
 
     for i in range(attempts):
-        # Try send HttpWebRequest
-        try:
-            response = web_session.post(url=req_url, data=post_content, headers=request_header, timeout=15,
-                                        proxies=curr_proxy_tuple)
-            function_resp['status'] = True
+        # Proxy tuple
+        curr_proxy_tuple = None
+        proxy_type = get_proxy_type(proxy, 3)
+        if proxy_type is not 'DEAD':
+            curr_proxy_tuple = {"https": "{proxy_type}://{ip_port}".format(proxy_type=proxy_type.lower(), ip_port=proxy),
+                                "http": "{proxy_type}://{ip_port}".format(proxy_type=proxy_type.lower(), ip_port=proxy)}
+        else:
+            continue
 
-            # Check response to know whether vote was counted or not
-            vote_resp_code = find_between(str(response.content), '"res":', ',')
+        for i in range(attempts):
+            # Try send HttpWebRequest
+            try:
+                response = web_session.post(url=req_url, data=post_content, headers=request_header, timeout=15, proxies=curr_proxy_tuple)
+                function_resp['status'] = True
 
-            # Response code 1 means the vote was successfully counter
-            if vote_resp_code.__contains__("1") is False:
+                # Check response to know whether vote was counted or not
+                vote_resp_code = find_between(str(response.content), '"res":', ',')
+
+                # Response code 1 means the vote was successfully counter
+                if vote_resp_code.__contains__("1") is False:
+                    function_resp['status'] = False
+                    function_resp['error_desc'] = "Request succeed but somehow vote was not counted :( Are you trying to vote twice from the same IP?"
+                    continue
+
+            except Exception as e:
+                function_resp['error_desc'] = str(e.message)
                 function_resp['status'] = False
-                function_resp['error_desc'] = "Request succeed but somehow vote was not counted :( Are you trying to vote twice from the same IP?"
-                continue
-
-        except Exception as e:
-            function_resp['error_desc'] = str(e.message)
-            function_resp['status'] = False
 
     return function_resp
 
-def task
 
 # Main function
 def main():
@@ -98,51 +100,56 @@ def main():
         for proxy in proxies:
             proxy_array.append(proxy.replace('\n', ''))
 
-    # Check socks
-    index = 1
-    valid_proxies = 0
-    invalid_proxies = 0
-    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
-        future_socks = {executor.submit(get_proxy_type, socks, 3): socks for socks in proxy_array}
+    # # Check socks
+    # index = 1
+    # valid_proxies = 0
+    # invalid_proxies = 0
+    # with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+    #     future_socks = {executor.submit(get_proxy_type, socks, 3): socks for socks in proxy_array}
+    #
+    #     # Wait for threads to finish
+    #     for future in concurrent.futures.as_completed(future_socks):
+    #         socks = future_socks[future]
+    #         result = future.result()
+    #         if result is 'DEAD':
+    #             invalid_proxies += 1
+    #         else:
+    #             invalid_proxies += 1
+    #         print("{index}. {proxy} -> {type}".format(index=str(index), proxy=socks, type=result))
+    #         index += 1
+    # ### Print Stats ###
+    # print("===== STATS =====")
+    # print("Total number of proxies: {proxies_number}".format(proxies_number=str(len(proxy_array))))
+    # print("Success votes: {success_votes}".format(success_votes=str(valid_proxies)))
+    # print("Failed votes: {failed_votes}".format(failed_votes=str(invalid_proxies)))
 
-        # Wait for threads to finish
-        for future in concurrent.futures.as_completed(future_socks):
-            socks = future_socks[future]
-            result = future.result()
-            if result is 'DEAD':
-                invalid_proxies += 1
-            else:
-                invalid_proxies += 1
-            print("{index}. {proxy} -> {type}".format(index=str(index), proxy=socks, type=result))
-            index += 1
-    ### Print Stats ###
-    print("===== STATS =====")
-    print("Total number of proxies: {proxies_number}".format(proxies_number=str(len(proxy_array))))
-    print("Success votes: {success_votes}".format(success_votes=str(valid_proxies)))
-    print("Failed votes: {failed_votes}".format(failed_votes=str(invalid_proxies)))
-
-
-    return
     # Loop through every proxy and try to vote
-    successVotes = 0
-    failVotes = 0
-    for i in range(len(proxy_array)):
-        st = datetime.datetime.fromtimestamp(time.time()).strftime('%H:%M:%S.%f')[:-3]
-        sys.stdout.write("[{timestamp}] {index}. Voting from {proxy}...".format(timestamp=st, index=str(i + 1),
-                                                                                proxy=proxy_array[i]))
-        vote_result = try_vote(contest_id, vote_id, requestUrl, proxy_array[i])
-        if vote_result['status'] is True:
-            successVotes += 1
-            sys.stdout.write("SUCCESS\n")
-        else:
-            failVotes += 1
-            sys.stdout.write("FAILED - {reason}\n".format(reason=vote_result['error_desc']))
+    success_votes = 0
+    fail_votes = 0
+    index = 0
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+        work_requests = {executor.submit(try_vote, contest_id=contest_id, vote_id=vote_id, req_url=request_url, attempts=3, proxy=socks): socks for socks in proxy_array}
+        # Wait for threads to finish
+        for future in concurrent.futures.as_completed(work_requests):
+            socks = work_requests[future]
+            result = future.result()
+
+            st = datetime.datetime.fromtimestamp(time.time()).strftime('%H:%M:%S.%f')[:-3]
+            sys.stdout.write("[{timestamp}] {index}. {proxy} -> ".format(timestamp=st, index=str(index + 1), proxy=socks))
+            if result['status'] is True:
+                success_votes += 1
+                sys.stdout.write("SUCCESS\n")
+            else:
+                fail_votes += 1
+                sys.stdout.write("FAILED - {reason}\n".format(reason=result['error_desc']))
+            index += 1
 
     # Print final stats
     print("===== STATS =====")
     print("Total number of proxies: {proxies_number}".format(proxies_number=str(len(proxy_array))))
-    print("Success votes: {success_votes}".format(success_votes=str(successVotes)))
-    print("Failed votes: {failed_votes}".format(failed_votes=str(failVotes)))
+    print("Success votes: {success_votes}".format(success_votes=str(success_votes)))
+    print("Failed votes: {failed_votes}".format(failed_votes=str(fail_votes)))
 
 
 # Entry point
